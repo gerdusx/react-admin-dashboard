4bac621b3968f583d604847ace02534efd5811d6
Gerdus Marais <gerdusx@gmail.com>
2023-08-04 15:13:07 +0200

    Topbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
-import React from 'react';
+import TopBar from "./scenes/global/TopBar";
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, Theme, ThemeProvider } from '@mui/material';
-import './App.css';
+import { CssBaseline, ThemeProvider } from '@mui/material';
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -10,7 +9,9 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <main className='content'></main>
+          <main className='content'>
+            <TopBar ></TopBar>
+          </main>
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
